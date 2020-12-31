108435b2c2273fe9cfb26bb52d16c83c357c68b5
Kyle Givler <xvegan88x@gmail.com>
2020-12-30 21:25:25 -0500

    Add LoadAllEntries()

--- TimeTrackerLibrary/Data/EntryData.cs
+++ TimeTrackerLibrary/Data/EntryData.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Threading.Tasks;
 using TimeTrackerLibrary.DataAccess;
 using TimeTrackerLibrary.Models;
@@ -33,6 +34,17 @@ namespace TimeTrackerLibrary.Data
         {
             var entries = await dataAccess.LoadData<EntryModel, dynamic>("dbo.spEntry_GetAll", new { });
 
+            foreach (var e in entries)
+            {
+                // TODO fix this with a join or something!
+                var p = await dataAccess.QueryRawSQL<ProjectModel, dynamic>("SELECT * FROM Project WHERE ID = @Id", new { Id = e.ProjectId });
+                e.Project = p.First();
+                var c = await dataAccess.QueryRawSQL<CategoryModel, dynamic>("SELECT * FROM Category WHERE ID = @Id", new { Id = e.Project.CategoryId });
+                e.Project.Category = c.First();
+                var s = await dataAccess.QueryRawSQL<SubcategoryModel, dynamic>("SELECT * FROM Category WHERE ID = @Id", new { Id = e.Project.SubcategoryId });
+                e.Project.Subcategory = s.FirstOrDefault();
+            }
+            
             return entries;
         }
     }
