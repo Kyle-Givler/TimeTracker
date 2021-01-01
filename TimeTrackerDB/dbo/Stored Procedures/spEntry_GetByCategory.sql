54764e78ec91bb764616197a0341ba992ffe8025
Kyle Givler <xvegan88x@gmail.com>
2021-01-01 18:32:33 -0500

    Stored Procs for getting Entries

---
+++ TimeTrackerDB/dbo/Stored Procedures/spEntry_GetByCategory.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spEntry_GetByCategory]
+	@CategoryId int
+AS
+begin
+
+	set nocount on;
+
+	select [e].[Id], [e].[ProjectId], [e].[HoursSpent], [e].[Date], [e].[Notes]
+	from entry e
+	inner join Project p on e.ProjectId = p.Id
+	inner join Category c on p.CategoryId = c.Id
+	where p.CategoryId = @CategoryId;
+
+end
---
+++ TimeTrackerDB/dbo/Stored Procedures/spEntry_GetBySubcategory.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spEntry_GetBySubcategory]
+	@SubcategoryId int
+AS
+begin
+
+	set nocount on;
+
+	select [e].[Id], [e].[ProjectId], [e].[HoursSpent], [e].[Date], [e].[Notes]
+	from entry e
+	inner join Project p on e.ProjectId = p.Id
+	inner join Subcategory s on p.SubcategoryId = s.Id
+	where p.SubcategoryId = @SubcategoryId;
+
+end
